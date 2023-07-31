826c8fe5bd6ccdcb23f5060e6bf3f848300ee71e
xEliezer <layugeliezerbongco16@gmail.com>
2023-07-31 23:06:25 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import Block from './components/Block';
 
 function App() {
+
+  const [state, setState] = useState(Array(9).fill(null));
+  const [currentTurn, setCurrentTurn] = useState("X");
+
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(let i=0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if(state[a] !== null && state[a] === state[b] && state[a] === state[c])
+        return true;
+    }
+    return false;
+  };
+
+  const isDraw = (state: any[]) => {
+    return state.every(cell => cell !== null) && !checkWinner(state);
+  };
+
+  const handleBlockClick = (index: number) => {
+    const stateCopy = Array.from(state);
+
+    if(stateCopy[index] !== null) return;
+    stateCopy[index] = currentTurn;
+
+    setState(stateCopy);
+
+    const win = checkWinner(stateCopy);
+
+    if (win) {
+      alert(`${currentTurn} won the game`);
+    } else if (isDraw(stateCopy)) {
+      alert("It's a draw!");
+    } else {
+      setCurrentTurn(currentTurn === "X" ? "O" : "X");
+    }
+  };
+
+  useEffect(() => {
+    if (isDraw(state)) {
+      alert("It's a draw!");
+    }
+  }, [state]);
+
+  const resetGame = () => {
+    setState(Array(9).fill(null));
+    setCurrentTurn("X");
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Tic Tac Toe</h1>
+        <div className="board">
+          <div className="row">
+            <Block onClick={() => handleBlockClick(0)} value={state[0]}/>
+            <Block onClick={() => handleBlockClick(1)} value={state[1]}/>
+            <Block onClick={() => handleBlockClick(2)} value={state[2]}/>
+          </div>
+          <div className="row">
+            <Block onClick={() => handleBlockClick(3)} value={state[3]}/>
+            <Block onClick={() => handleBlockClick(4)} value={state[4]}/>
+            <Block onClick={() => handleBlockClick(5)} value={state[5]}/>
+          </div>
+          <div className="row">
+            <Block onClick={() => handleBlockClick(6)} value={state[6]}/>
+            <Block onClick={() => handleBlockClick(7)} value={state[7]}/>
+            <Block onClick={() => handleBlockClick(8)} value={state[8]}/>
+          </div>
+        </div>
+        <button onClick={resetGame} className='btn'>Reset</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Block.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface BlockProps {
+    value?: string | null;
+    onClick?: () => void;
+}
+
+const Block: React.FC<BlockProps> = (props) => {
+    return <div onClick={props.onClick} className="block">
+            {props.value}
+            </div>;
+};
+
+export default Block; 
